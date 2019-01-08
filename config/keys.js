af7d0b51ec3f987b5a9fbbc6d8e2646a77b477d6
vegeta1509 <vegeta1509@users.noreply.github.com>
2019-01-08 19:59:16 +0530

    minor

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '478659425994-6n1h9rqijvakt11qr7ve244s0v6vm4th.apps.googleusercontent.com',
+            clientSecret: '_hJ6pIsBdhkMYs2zXpRWlF2t'
+        },
+        mongodb: {
+            dbURI: 'mongodb://vegeta1509:Mist12I@ds113445.mlab.com:13445/mist-19'
+        },
+        session: {
+            cookieKey: 'mistisawesome'
+        }
+};
