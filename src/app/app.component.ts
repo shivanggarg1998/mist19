5b5cc8f0645d6c42b87fd0f32d7f6fad2f289cb1
vegeta1509 <vegeta1509@users.noreply.github.com>
2019-01-13 22:22:18 +0530

    side bar created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,52 @@
 import { Component } from '@angular/core';
+import { AdminService } from './admin.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Http, Response, RequestOptions, Headers} from '@angular/http';
+declare var require: any;
+require('../assets/js/main/jquery.min.js');
+require('../assets/js/main/bootstrap.bundle.min.js');
+require('../assets/js/app.js');
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+  "../assets/css/icons/icomoon/styles.css",
+  "../assets/css/bootstrap.min.css",
+  "../assets/css/bootstrap_limitless.min.css",
+  "../assets/css/layout.min.css",
+  "../assets/css/components.min.css",
+  "../assets/css/colors.min.css"
+]
 })
 export class AppComponent {
-  title = 'app';
+  player: any;
+  playerRank: any;
+  title = 'Mist';
+  constructor(
+    
+    private adminService: AdminService,
+    private router: Router
+  ) { }
+  ngOnInit() {
+    
+    this.adminService.fetchUser().subscribe(Player => {
+      this.player = Player;
+      if(Player.isNotBan==false){
+        this.router.navigate(['/']);}
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
+    this.adminService.fetchPlayers().subscribe(playerList => {
+      this.playerRank= playerList.current_rank;
+    },
+    err => {
+      console.log(err)
+      return false
+    })
+
+
+  }
 }
