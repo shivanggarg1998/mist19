7304a65ace7c4e2fd22a3ce8ee558aa7068a4240
shivanggarg1998 <shivanggarg1998@gmail.com>
2020-03-19 01:48:29 +0530

    g auth

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,8 @@
 
 module.exports = {
         google: {
-            clientID: '369743875191-e2sa0o81emuj6s1cf47qqpeqkpuktra8.apps.googleusercontent.com',
-            clientSecret: 's1Zm7pTLytrDoVBlUkfuoukC'
+            clientID: '555963106003-73opk82qm706p4feqhiqkogh6r6o2dvq.apps.googleusercontent.com',
+            clientSecret: 'jsnvo0DDE3TB7Qb8DhT_8-RW'
         },
         mongodb: {
             dbURI: 'mongodb://shivanggarg:mist2020@ds263448.mlab.com:63448/mist'
