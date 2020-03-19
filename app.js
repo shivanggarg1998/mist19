c46e28f4ab442d7051f42f6900dddb2ff9977d1b
shivanggarg1998 <shivanggarg1998@gmail.com>
2020-03-19 13:05:17 +0530

    remove limiter

--- app.js
+++ app.js
@@ -25,11 +25,7 @@ app.use(cookieSession({
 }));
 app.enable("trust proxy"); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
  
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
-});
-app.use(limiter);
+
 require('./config/passport-setup')(passport);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
